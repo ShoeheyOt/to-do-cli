b42888fd28a6b0d3f977d97081a2bfd15fcf11a6
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-08-31 22:44:32 -0700

    condition

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ fn main()->io::Result<()> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
 
-    println!("what you entered is: {}", input);
+    if input.trim() == "exit" {
+        println!("Are you sure?");
+    } else {
+        println!("what you entered is: {}", input);
+    }
 
     Ok(())
 
