1ae097f981f6e86b479377c720d9d3765fa80d51
hirohata <hirohatatro@gmail.com>
2024-09-07 06:06:50 -0700

    add an enum

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,27 @@ use file_operation::{find_index_opt, try_add_lines, try_read_lines, try_update_f
 use std::io;
 use std::io::prelude::*;
 
+enum Mode {
+    DISPLAY,
+    ADD,
+    DELETE,
+    EXIT,
+    UNDEFINED,
+}
+
+impl From<&str> for Mode {
+    fn from(value: &str) -> Self {
+        println!("{:?}", value);
+        match value {
+            "1" => Mode::DISPLAY,
+            "2" => Mode::ADD,
+            "3" => Mode::DELETE,
+            "4" => Mode::EXIT,
+            _ => Mode::UNDEFINED,
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut input = String::new();
 
@@ -15,72 +36,79 @@ fn main() -> io::Result<()> {
         input.clear();
         io::stdin().read_line(&mut input)?;
 
-        if input.trim() == "1" {
-            match try_read_lines("todo.txt") {
-                Ok(lines) => {
-                    if lines.is_empty() {
-                        println!("nothing to do for now \n");
-                        io::stdout().flush()?;
-                    } else {
-                        for todo in lines {
-                            println!("{}", todo)
+        let mode = Mode::from(input.trim());
+
+        match mode {
+            Mode::DISPLAY => {
+                match try_read_lines("todo.txt") {
+                    Ok(lines) => {
+                        if lines.is_empty() {
+                            println!("nothing to do for now \n");
+                            io::stdout().flush()?;
+                        } else {
+                            for todo in lines {
+                                println!("{}", todo)
+                            }
                         }
                     }
+                    Err(why) => eprintln!("Error reading file : {}", why),
                 }
-                Err(why) => eprintln!("Error reading file : {}", why),
+                println!("\n");
+
+                continue;
             }
-            println!("\n");
 
-            continue;
-        }
-
-        if input.trim() == "2" {
-            let mut new_todo = String::new();
-            println!("please input new todo >> \n");
-            io::stdout().flush()?;
-            io::stdin().read_line(&mut new_todo)?;
-
-            match try_add_lines(&new_todo) {
-                Err(why) => eprintln!("couldn't add {} : {}", new_todo, why),
-                Ok(_) => println!("success!\n"),
-            }
-            continue;
-        }
-
-        if input.trim() == "3" {
-            if try_read_lines("todo.txt")?.len() == 0 {
-                println!("Nothing to delete now \n");
+            Mode::ADD => {
+                let mut new_todo = String::new();
+                println!("please input new todo >> \n");
                 io::stdout().flush()?;
-            } else {
-                println!("which one? >");
+                io::stdin().read_line(&mut new_todo)?;
 
-                if let Ok(vector) = try_read_lines("todo.txt") {
-                    for todo in vector {
-                        println!("{}", todo)
-                    }
+                match try_add_lines(&new_todo) {
+                    Err(why) => eprintln!("couldn't add {} : {}", new_todo, why),
+                    Ok(_) => println!("success!\n"),
+                }
+                continue;
+            }
+            Mode::DELETE => {
+                if try_read_lines("todo.txt")?.len() == 0 {
+                    println!("Nothing to delete now \n");
+                    io::stdout().flush()?;
                 } else {
-                    eprintln!("couldn't read file!!!")
-                }
+                    println!("which one? >");
 
-                let mut delete_todo = String::new();
-
-                io::stdin().read_line(&mut delete_todo)?;
-
-                match find_index_opt(&delete_todo) {
-                    Some(_) => {
-                        try_update_file(&delete_todo)?;
-                        println!("successfully delete {}", delete_todo);
+                    if let Ok(vector) = try_read_lines("todo.txt") {
+                        for todo in vector {
+                            println!("{}", todo)
+                        }
+                    } else {
+                        eprintln!("couldn't read file!!!")
                     }
-                    None => println!("not found\n"),
+
+                    let mut delete_todo = String::new();
+
+                    io::stdin().read_line(&mut delete_todo)?;
+
+                    match find_index_opt(&delete_todo) {
+                        Some(_) => {
+                            try_update_file(&delete_todo)?;
+                            println!("successfully delete {}", delete_todo);
+                        }
+                        None => println!("not found\n"),
+                    }
                 }
+
+                continue;
             }
 
-            continue;
-        }
+            Mode::EXIT => {
+                println!("See you!");
+                break;
+            }
 
-        if input.trim() == "4" {
-            println!("See you!");
-            break;
+            Mode::UNDEFINED => {
+                continue;
+            }
         }
     }
     Ok(())
